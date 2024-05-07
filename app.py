658dc834bab29b6835356349c60256c68d046529
Ahmed Elzayat <113690422+AhmeddEmad7@users.noreply.github.com>
2024-05-07 15:21:30 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,27 +1,35 @@
-from flask import Flask, request, jsonify, render_template
+import streamlit as st
 import pandas as pd
-import pickle
 import numpy as np
+import pickle
+import sklearn
 
-app = Flask(__name__)
+st.title('Predicting a Startup’s Acquisition Status')
+st.image('company-status-active.png')
 
-# Load the model and data
+
+#Load data
 df = pd.read_csv('Cleaned_data.csv')
-model = pickle.load(open('Model.pkl', 'rb'))
+model = pd.read_pickle('Model.pkl')
 
-@app.route('/')
-def home():
-    return render_template('index.html')
+#App
 
-@app.route('/predict', methods=['POST'])
-def predict():
-    data = request.get_json()
 
-    new_data = pd.DataFrame(data, index=[0])
 
-    status = model.predict(new_data)
+funding_rounds = st.number_input('funding rounds', min_value=df['funding_rounds'].min(), max_value=df['funding_rounds'].max())
+funding_total_usd = st.number_input('funding total usd', min_value=df['funding_total_usd'].min(), max_value=df['funding_total_usd'].max())
+lat = st.number_input('latitude', min_value=df['lat'].min(), max_value=df['lat'].max())
+lng = st.number_input('longitude', min_value=df['lng'].min(), max_value=df['lng'].max())
+relationships = st.number_input('relationships', min_value=df['relationships'].min(), max_value=df['relationships'].max())
 
-    return jsonify({'status': status.tolist()})
+new_data = {'funding_rounds':funding_rounds,'funding_total_usd':funding_total_usd,
+            'lat':lat,'lng':lng ,'relationships':relationships }
 
-if __name__ == '__main__':
-    app.run(debug=True)
+new_data = pd.DataFrame(new_data,index=[0])
+
+status = model.predict(new_data)
+
+# Output
+if st.button('Predict'):
+    st.markdown('## Status:')
+    st.markdown(status)
